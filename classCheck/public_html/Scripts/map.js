8f8cc2a6287b8b055a59ff6d49bfac27094f3624
Dimda <daviskemlers@gmail.com>
2015-06-04 15:38:38 +0900

    commit

--- classCheck/public_html/Scripts/map.js
+++ classCheck/public_html/Scripts/map.js
@@ -6,12 +6,10 @@ $(function(){
             $(this).siblings().find(".floornum").removeClass("hide_floornum");
             $(this).toggleClass("zoom");
             $(this).siblings().removeClass("zoom");
-            /*$(this).find(".class").css(
-                {
-                    'word-wrap' : 'break-word',
-                    'letter-spacing' : '300px'
-                });*/
         });
+    $(window).scroll(function(){
+        $(".class").css("color", "blue");
+    });   
 });
 
 
