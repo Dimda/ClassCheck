797fd972aacf346889778c5f386d007e48741c44
daviskemlers@gmail.com <b4100@oic.jp>
2015-06-24 13:42:13 +0900

    回転追加

--- classCheck/public_html/Scripts/map.js
+++ classCheck/public_html/Scripts/map.js
@@ -1,15 +1,20 @@
 
 $(function(){
     $(".floor").click(
-        function(){
-            $(this).find(".floornum").toggleClass("hide_floornum");
-            $(this).siblings().find(".floornum").removeClass("hide_floornum");
-            $(this).toggleClass("zoom");
-            $(this).siblings().removeClass("zoom");
+        function(event){
+	        if($(event.target).hasClass("class")){
+		        $(event.target).css("transform", "rotateY(360deg)");
+            $(event.target).text("TEST");
+
+		        return;
+	        }
+          $(this).find(".floornum").toggleClass("hide_floornum");
+          $(this).siblings().find(".floornum").removeClass("hide_floornum");
+          $(this).toggleClass("zoom");
+          $(this).siblings().removeClass("zoom");
         });
     $(window).scroll(function(){
         $(".class").css("color", "blue");
-    });   
+
+    });
 });
-
-
