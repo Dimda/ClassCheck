572db8db2e2dfcef4f0d1c89bcd324f42e92f6cc
44x1carbon <rad_bump_yoshi@yahoo.co.jp>
2015-06-25 23:54:43 +0900

    授業表示状態から教室番号に戻す時も回転するように修正

--- classCheck/public_html/Scripts/map.js
+++ classCheck/public_html/Scripts/map.js
@@ -2,23 +2,23 @@ $(function(){
     $(".floor").click(
         function(event){
           var classroom = $(event.target);
-	        if(classroom.hasClass("class") && !classroom.hasClass("activated")){
-            classroom.addClass("activated");
-		        classroom.css("transform", "rotateY(360deg)");
-            classroom.text("TEST");
-		        return;
-	        }else if (classroom.hasClass("activated")) {
-	          classroom.removeClass("activated");
-            classroom.css("transform", "rotateY(360deg)");
-            classroom.text(classroom.attr('id'));
-            return;
-	        }
-          $(this).find(".floornum").toggleClass("hide_floornum");
-          $(this).siblings().find(".floornum").removeClass("hide_floornum");
-          $(this).toggleClass("zoom");
-          $(this).siblings().removeClass("zoom");
+	           if(classroom.hasClass("class") && !classroom.hasClass("activated")){
+                  classroom.addClass("activated");
+  		        classroom.css("transform", "rotateY(360deg)");
+                  classroom.text("TEST");
+  		        return;
+	           }else if (classroom.hasClass("activated")) {
+	             classroom.removeClass("activated");
+                  classroom.css("transform", "rotateY(0deg)");
+                  classroom.text(classroom.attr('id'));
+                  return;
+	           }
+                $(this).find(".floornum").toggleClass("hide_floornum");
+                $(this).siblings().find(".floornum").removeClass("hide_floornum");
+                $(this).toggleClass("zoom");
+                $(this).siblings().removeClass("zoom");
         });
-    $(window).scroll(function(){
+      $(window).scroll(function(){
         $(".class").css("color", "blue");
     });
 });
