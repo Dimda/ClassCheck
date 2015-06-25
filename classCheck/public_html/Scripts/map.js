60ec9b2de98c279a082c513bd0566388280f3c22
44x1carbon <rad_bump_yoshi@yahoo.co.jp>
2015-06-26 00:23:01 +0900

    map.jsの各処理を関数化 .floorをクリックして戻した場合元の状態の戻す処理を実装

--- classCheck/public_html/Scripts/map.js
+++ classCheck/public_html/Scripts/map.js
@@ -3,22 +3,45 @@ $(function(){
         function(event){
           var classroom = $(event.target);
 	           if(classroom.hasClass("class") && !classroom.hasClass("activated")){
-                  classroom.addClass("activated");
-  		        classroom.css("transform", "rotateY(360deg)");
-                  classroom.text("TEST");
-  		        return;
+                    addActivated(classroom);
+                    return;
 	           }else if (classroom.hasClass("activated")) {
-	             classroom.removeClass("activated");
-                  classroom.css("transform", "rotateY(0deg)");
-                  classroom.text(classroom.attr('id'));
+	             removeActivated(classroom);
                   return;
 	           }
-                $(this).find(".floornum").toggleClass("hide_floornum");
-                $(this).siblings().find(".floornum").removeClass("hide_floornum");
-                $(this).toggleClass("zoom");
-                $(this).siblings().removeClass("zoom");
+                toggleZoom(this);
+                resetClassRoom();
         });
       $(window).scroll(function(){
         $(".class").css("color", "blue");
     });
 });
+
+var addActivated = function(classroom){
+      classroom.addClass("activated");
+      classroom.css("transform", "rotateY(360deg)");
+      classroom.text("TEST");
+      
+}
+
+var removeActivated = function(classroom){
+      classroom.removeClass("activated");
+      classroom.css("transform", "rotateY(0deg)");
+      classroom.text(classroom.attr('id'));
+     
+}
+
+var toggleZoom = function(elem){
+      $(elem).find(".floornum").toggleClass("hide_floornum");
+      $(elem).siblings().find(".floornum").removeClass("hide_floornum");
+      $(elem).toggleClass("zoom");
+      $(elem).siblings().removeClass("zoom");
+}
+
+var resetClassRoom = function(){
+    $(".activated").each(function(index,elem){
+                      $(elem).removeClass("activated");
+                      $(elem).text($(elem).attr('id'));
+     
+      });
+}
