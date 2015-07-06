55fc5fb2726cc334dfb9033832b1b870da55fd94
daviskemlers@gmail.com <b4100@oic.jp>
2015-07-06 13:12:59 +0900

    教室でユーザーと目的を改行

--- classCheck/public_html/Scripts/map.js
+++ classCheck/public_html/Scripts/map.js
@@ -3,27 +3,40 @@ $(function(){
         function(event){
           var classroom = $(event.target);
 	           if(classroom.hasClass("class") && !classroom.hasClass("activated")){
-                    addActivated(classroom);
-                    return;
+               addActivated(classroom);
+               return;
 	           }else if (classroom.hasClass("activated")) {
 	             removeActivated(classroom);
-                  return;
+               return;
 	           }
                 toggleZoom(this);
                 resetClassRoom();
         });
 });
 
+var obj = {"8-A":{"purpose":"プレゼンテーション技法","user":"新井"}};
+
+
+
 var addActivated = function(classroom){
+      var id = classroom.attr('id');
       classroom.addClass("activated");
-      classroom.css("transform", "rotateY(360deg)");
-      classroom.text("TEST");
+      classroom.css({
+        "transform" : "rotateY(360deg)",
+        "padding" : "0 0 6vmin 0",
+        "font-size" : "4vmin"
+      });
+      classroom.text(obj[id]["purpose"] + '\n' + obj[id]["user"]);
 
 }
 
 var removeActivated = function(classroom){
       classroom.removeClass("activated");
-      classroom.css("transform", "rotateY(0deg)");
+      classroom.css({
+        "transform" : "rotateY(0deg)",
+        "padding" : "6vmin 0 0 0",
+        "font-size" : "6vmin"
+      });
       classroom.text(classroom.attr('id'));
 
 }
