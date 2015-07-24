609971d9d85178bcbaec0186ac06c0440b38f990
daviskemlers@gmail.com <b4100@oic.jp>
2015-07-24 16:07:34 +0900

    空いてる、空いてないかどうかに色分
    け

--- classCheck/public_html/Scripts/map.js
+++ classCheck/public_html/Scripts/map.js
@@ -1,43 +1,82 @@
 $(function(){
-    $(".floor").click(
-        function(event){
-          var classroom = $(event.target);
-	           if(classroom.hasClass("class") && !classroom.hasClass("activated")){
-               addActivated(classroom);
-               return;
-	           }else if (classroom.hasClass("activated")) {
-	             removeActivated(classroom);
-               return;
-	           }
-                toggleZoom(this);
-                resetClassRoom();
-        });
+
+  var classrooms = {};
+  initClassrooms(classrooms);
+
+  classrooms["8-A"].aviability = false;
+  classrooms["8-A"].purpose = "プレゼンテーション技法";
+  classrooms["8-A"].user = "新井";
+
+  checkClassrooms(classrooms);
+
+  $(".floor").click(
+      function(event){
+        var target = $(event.target);
+	          if(target.hasClass("class") && !target.hasClass("activated")){
+              addActivated(target, classrooms);
+              return;
+	          }else if (target.hasClass("activated")) {
+	            removeActivated(target);
+              return;
+	          }
+              toggleZoom(this);
+              resetClassRoom();
+  });
 });
 
-var obj = {"8-A":{"purpose":"プレゼンテーション技法","user":"新井"}};
-
-
-
-var addActivated = function(classroom){
-      var id = classroom.attr('id');
-      classroom.addClass("activated");
-      classroom.css({
-        "transform" : "rotateY(360deg)",
-        "padding" : "0 0 6vmin 0",
-        "font-size" : "4vmin"
-      });
-      classroom.text(obj[id]["purpose"] + '\n' + obj[id]["user"]);
-
+function nextChar(c) {
+    return String.fromCharCode(c.charCodeAt(0) + 1);
 }
 
-var removeActivated = function(classroom){
-      classroom.removeClass("activated");
-      classroom.css({
+function checkClassrooms(classrooms){
+  for(i = 2; i <= 8; i++){
+    var char = 'A';
+    for(j = 0; j < 4; j++){
+      if(classrooms[i + "-" + char].aviability == false){
+        $("#" + i + "-" + char).addClass("unavailable");
+      }
+      char = nextChar(char);
+    }
+  }
+}
+
+var initClassrooms = function(classrooms){
+  for(i = 2; i <= 8; i++){
+    var char = 'A';
+    for(j = 0; j < 4; j++){
+      classrooms[i + "-" + char]={};
+      classrooms[i + "-" + char].aviability = true;
+      char = nextChar(char);
+    }
+  }
+  return classrooms;
+}
+
+
+var addActivated = function(target, classrooms){
+      var id = target.attr('id');
+      target.addClass("activated");
+      target.css({"transform" : "rotateY(360deg)"});
+
+      if(classrooms[id].aviability == false){
+        target.css({
+          "font-size" : "4vmin",
+          "padding" : "0 0 6vmin 0"
+        });
+        target.text(classrooms[id]["purpose"] + '\n' + classrooms[id]["user"]);
+      }else{
+        target.text("予約");
+      }
+}
+
+var removeActivated = function(target){
+      target.removeClass("activated");
+      target.css({
         "transform" : "rotateY(0deg)",
         "padding" : "6vmin 0 0 0",
         "font-size" : "6vmin"
       });
-      classroom.text(classroom.attr('id'));
+      target.text(target.attr('id'));
 
 }
 
@@ -52,6 +91,5 @@ var resetClassRoom = function(){
     $(".activated").each(function(index,elem){
                       $(elem).removeClass("activated");
                       $(elem).text($(elem).attr('id'));
-
       });
 }
